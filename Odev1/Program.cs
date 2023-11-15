5b186c3a0ededf58f7053fa5c80648cbd01f8d48
GokhanYmn <ymngokhan7@gmail.com>
2023-11-15 19:55:31 +0300

    ilkyukleme
    Switch case le yapılmış hesap makinasının ilk  dosyaları

---
+++ Odev1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char islem;
+            double sayi1, sayi2, sonuc;
+            Console.WriteLine("1. Sayı giriniz");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2. Sayı giriniz");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+            sonuc = 0;
+
+            Console.WriteLine("İşlem seçiniz : [+|-|*|/] çıkış yapmak için boş geçiniz");
+            islem = Convert.ToChar(Console.ReadLine());
+
+
+
+            switch (islem)
+            {
+
+                case '+':
+                    sonuc = sayi1 + sayi2;
+                    Console.WriteLine(sonuc);
+                    break;
+                case '-':
+                    sonuc = sayi1 - sayi2;
+                    Console.WriteLine(sonuc);
+                    break;
+                case '*':
+                    sonuc = sayi1 * sayi2;
+                    Console.WriteLine(sonuc);
+                    break;
+                case '/':
+                    sonuc = sayi1 / sayi2;
+                    Console.WriteLine(sonuc);
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+
+
+            }
+        }
+    }
+}
